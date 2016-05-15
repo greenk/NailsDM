e4a9980d724aa4f784d2b70649e5bfc4e8b655ce
greenk <binhsuse@gmail.com>
2016-05-15 13:17:23 -0500

    update customer card
    update customer card

--- themes/material/base/assets/nailsdms/js/customer-card.js
+++ themes/material/base/assets/nailsdms/js/customer-card.js
@@ -19,7 +19,7 @@ var CustomerCard = {
 
     build: function() {
         //Calling ajax to get JSON data
-        console.log("this.$myCanvas", this.$myCanvas);
+        //console.log("this.$myCanvas", this.$myCanvas);
         var _self = this;
         $.ajax({
             url: "../../assets/data/customer-card.json",
@@ -46,17 +46,29 @@ var CustomerCard = {
                 // REMEMBER store json data to our canvas element
                 $('#customer-card-canvas').data('customer_and_menu', json);
 
-                _self.buildCustomerCard(json);
+                _self.buildCustomerObject(json);
 
             }
         });
 
         return this;
     },
-
-    buildCustomerCard: function(json) {
+    /**
+     * Use this function to construct object data for tracking employees while they work on customer
+     * - we keep adding, deleting, editing customer service on object_service_array
+     * - our object data is  object_service_array = [object_service, object_service, etc]
+     * - each object_service = {
+     *                          name : <name of object service>,
+     *                          index : <position of object service in object_service_array>,
+     *                          work_array : [<service object which is copied from our work_tb in json file>],
+     *                          $container_class_name: <string - store class name of service container which appear on html page>
+      *                        }
+     * @param json a json we got after make an ajax call
+     * @returns {updated this.customer_data_and_menu, this.object_service_array}
+     */
+    buildCustomerObject: function(json) {
         this.customer_data_and_menu = json;
-        this.service_type_track = [];
+        this.object_service_array = [];
         var _self = this;
         var _td = json;
 
@@ -68,21 +80,47 @@ var CustomerCard = {
         if (_td.customer.work_tb.length > 0) {
             var _nail_tech_name_array = [];
             var _request_nail_tech_array = [];
-            var _service_type_track = [];
+            var _object_service_array = [];
+            var _work_category_array = [];
+
             for (var i = 0; i < _td.customer.work_tb.length; i++){
                 // Get Nails tech list
                 if(_td.customer.work_tb[i].employee != null){
                     if( _td.customer.work_tb[i].employee.e_american_name != null){
                         _nail_tech_name_array.push(_td.customer.work_tb[i].employee.e_american_name);
                     } else{
-                        _nail_tech_name_array.push(_td.customer.work_tb[i].employee.e_first_name + _td.customer.work_tb[i].employee.e_last_name);
+                        _nail_tech_name_array.push(_td.customer.work_tb[i].employee.e_first_name + ' ' + _td.customer.work_tb[i].employee.e_last_name);
                     }
                 }
                 // Get request nail tech list
                 if(_td.customer.work_tb[i].request_for != null){
                     _request_nail_tech_array.push(_td.customer.work_tb[i].request_for);
                 }
-                //
+
+                var _index_value = $.inArray(_td.customer.work_tb[i].work_type.work_category, _work_category_array);
+                //console.log("my _index_value", _index_value);
+                if (_index_value == -1)
+                {
+                    _work_category_array.push(_td.customer.work_tb[i].work_type.work_category);
+
+                    // Create a new type of work service object and store it in an array
+                    var _object_service = {
+                        'name': _td.customer.work_tb[i].work_type.work_category,
+                        'index': _work_category_array.length - 1,
+                        'work_array': [],
+                        '$container_class_name': '',
+                    };
+                    _object_service.work_array.push(_td.customer.work_tb[i]);
+                    _object_service_array.push(_object_service);
+                }
+                else {
+                    // find where it is in _object_service_array and then push it in work_tb array
+                    // using index value
+                    _object_service_array[_index_value].work_array.push(_td.customer.work_tb[i]);
+                }
+
+                // Need to come with better idea
+                /*
                 switch (_td.customer.work_tb[i].work_type.work_category){
                     case 'hand':
                         if($.inArray(_td.customer.work_tb[i].work_type.work_category, _service_type_track) == -1){
@@ -107,7 +145,8 @@ var CustomerCard = {
                         $_first_col.find('input').attr('value', _td.customer.work_tb[i].work_type.work_name );
                         $_first_col.find('label').attr('for','customer-select-hand-service-' + _td.customer.work_tb[i].id );
 
-                         
+                        $_second_col.children('div').remove();
+
 
                         break;
                     case 'feet':
@@ -115,24 +154,37 @@ var CustomerCard = {
                     case 'waxing':
                         break;
                 }
+                */
             }
+
+            // After construct our object_service_array from json file,
+            // we call drawServiceTables to show services in object_service_array to screen
+            _self.drawServiceTables(_object_service_array);
+
+            console.log("My _object_service_array", _object_service_array);
+            this.object_service_array = _object_service_array;
+
             this.$myCanvas.children('.panel-body').find('.request-nail-tech').html('Request: ' + _request_nail_tech_array.join(', '));
             this.$myCanvas.children('.panel-body').find('.nail-tech-serve').html('Nails tech: ' + _nail_tech_name_array.join(', '));
         }
 
-
-
-
-
-
-
-
-
-
-
-
         return this;
     },
+
+    /**
+     * Draw html table using data from object_service_array
+     * - update $container_class_name for each object_service
+     * @param object_service_array
+     */
+    drawServiceTables: function (object_service_array) {
+        // TODO:
+        // for each object_service
+        // create a new service-type-container by cloning a service-type-container-template
+        // fill this new service-type-container  with data in object_service
+        // append this service-type-container to service-type-container-master
+        return this;
+    },
+
 
     events: function() {
 
